b330010b03097ad2d3231a56d2693d140c753d33
unoeufisenough <56774966+unoeufisenough@users.noreply.github.com>
2021-10-02 17:17:12 +0100

    last stuff

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/nav.css'
 import { NavLink } from 'react-router-dom'
 import { Grid, Box } from "@material-ui/core";
 
-function Nav() {
+function Nav({pageTitleName}) {
+
+   const [titleActive, setTitleActive] = useState("");
+   
+    
+
     return (
         <nav className='nav'>
             <Grid container >
@@ -18,10 +23,10 @@ function Nav() {
             
             
             <ul className="nav__options">
-                <li className="nav__option"><NavLink activeClassName="nav__optionActive" exact to='/'><Box className="nav__optionName" pr={2} pl={2}>Home</Box></NavLink></li>
-                <li className="nav__option"><NavLink activeClassName="nav__optionActive" to='/Work'><Box className="nav__optionName" pr={2} pl={2}>Work</Box></NavLink></li>
-                <li className="nav__option"><NavLink activeClassName="nav__optionActive" to='/Art'><Box className="nav__optionName" pr={2} pl={2}>Art</Box></NavLink></li>
-                <li className="nav__option"><NavLink activeClassName="nav__optionActive" to='/About'><Box className="nav__optionName" pr={2} pl={2}>About</Box></NavLink></li>
+                <li className="nav__option"><NavLink to='/'  isActive={(match, pageTitleName) => {if (!match) {return false;} console.log(pageTitleName.pathname)}} activeClassName="nav__optionActive" ><Box className="nav__optionName" pr={2} pl={2}>Home</Box></NavLink></li>
+                <li className="nav__option"><NavLink to='/Work'activeClassName="nav__optionActive" ><Box className="nav__optionName" pr={2} pl={2}>Work</Box></NavLink></li>
+                <li className="nav__option"><NavLink to='/Art' activeClassName="nav__optionActive" ><Box className="nav__optionName" pr={2} pl={2}>Art</Box></NavLink></li>
+                <li className="nav__option"><NavLink to='/About' activeClassName="nav__optionActive" ><Box className="nav__optionName" pr={2} pl={2}>About</Box></NavLink></li>
             </ul>
            
                 </Grid>
--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../style/pageTitle.css'
 
-function PageTitle() {
+function PageTitle(pageTitleName) {
+
     return (
         <div className="pageTitle">
             <div className="pageTitle__arrowWrap">
                 <button className="pageTitle__arrow"><span className="visually-hidden"></span></button>              
             </div>
-            <h2 className="pageTitle__h">The Title Selected will be there</h2>
+            <h2 className="pageTitle__h">Home {pageTitleName.pathname}</h2>
         </div>
     )
 }
